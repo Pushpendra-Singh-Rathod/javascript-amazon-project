21a18578019162210f641165f8da3f568460d77e
Pushpendra Singh <rathodpushpendra06@gmail.com>
2025-04-30 20:01:21 +0530

    added to selct quantity from dropdown

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity" data-product-id= "${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -59,8 +59,15 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
 
     button.addEventListener('click',()=>{
+    
        const productId= button.dataset.productId;
+        const selectElement = document.querySelector(
+          `.js-quantity[data-product-id="${productId}"]`
+        );
+       let selectValue=Number(selectElement.value);
        let matchingItem;
+      
+       
 
        cart.forEach((item)=>{
            if(productId===item.productId){
@@ -70,12 +77,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
        });
 
        if(matchingItem){
-        matchingItem.quantity+=1;
+        matchingItem.quantity+=selectValue;
        }
        else{
          cart.push({
         productId:productId,
-        quantity:1
+        quantity:selectValue
        });
        }
     
@@ -83,8 +90,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
        cart.forEach((item)=>{
          cartQuantity+=item.quantity;
        });
-
+      
        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-     
+      selectElement.value="1"
     });
 });
