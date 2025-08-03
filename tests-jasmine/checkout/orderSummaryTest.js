951fead7cb14b3c999ac72db102d232b842fedac
Pushpendra Singh <rathodpushpendra06@gmail.com>
2025-08-03 15:45:25 +0530

    convert products into classes

--- tests-jasmine/checkout/orderSummaryTest.js
+++
@@ -1,9 +0,0 @@
-import {renderOrderSummary} from "../../checkout/orderSummary.js";
-
-describe('test suite: renderOrderSummary', () => {
-    it('display the cart',() =>{
-      document.querySelector('.js-test-container').innerHTML = `
-      <div class="js-order-summary"></div>
-      `;
-    });
-});
